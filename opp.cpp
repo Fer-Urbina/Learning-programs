3fecee41650e2d702376a0ae5d8082ea77df432e
ufernando769 <ufernando769@gmail.com>
2023-03-23 00:57:21 -0600

    Program that calculates the n number of salaries entered by the user

--- opp.cpp
+++ opp.cpp
@@ -1,16 +1,55 @@
-/*
-    Created this file with the purpose of practicing object oriente programming (OPP) in C++
-*/
-
 #include <iostream>
+using namespace std;
 
-int percentages(int n, int resultado)
-{
-    int salarios[n];
-    
-}
+class SalaryCalculator {
+private:
+  int n;
+  double* salaries;
+  double totalSalary;
+public:
+  SalaryCalculator() {
+    n = 0;
+    salaries = NULL;
+    totalSalary = 0.0;
+  }
 
-int main ()
-{
-    return 0;
+  void getSalaryInput() {
+    cout << "Enter the number of salaries: ";
+    cin >> n;
+    salaries = new double[n];
+
+    for (int i = 0; i < n; i++) {
+      cout << "Enter salary " << i+1 << ": ";
+      cin >> salaries[i];
+      totalSalary += salaries[i];
+    }
+  }
+
+  void calculateIndividualSalary() {
+    for (int i = 0; i < n; i++) {
+      double individualSalary = salaries[i] * 0.3;
+      cout << "30% of salary " << i+1 << " (" << salaries[i] << "): " << individualSalary << endl;
+    }
+  }
+
+  void calculateTotalSalary() {
+    double totalIndividualSalary = totalSalary * 0.3;
+    cout << "30% of total salary (" << totalSalary << "): " << totalIndividualSalary << endl;
+  }
+
+  ~SalaryCalculator() {
+    if (salaries != NULL) {
+      delete[] salaries;
+      salaries = NULL;
+    }
+  }
+};
+
+int main() {
+  SalaryCalculator calculator;
+  calculator.getSalaryInput();
+  calculator.calculateIndividualSalary();
+  calculator.calculateTotalSalary();
+
+  return 0;
 }
